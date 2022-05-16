29dd76534326bb268937ee0d5d548b7173ce6b53
24 <4343@mail.ru>
2022-05-16 09:38:27 +0300

    1

---
+++ WpfLR2/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLR2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Window1 win1 = new Window1();
+            Window2 win2 = new Window2();
+        }
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            Window1 win1 = new Window1();
+            Window2 win2 = new Window2();
+            win1.Left = this.Left + this.ActualWidth - 10;
+            win1.Top = this.Top + this.ActualHeight - 10;
+        }
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 win2 = new Window2();
+            win2.Show();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 win1 = new Window1();
+            if (win1.IsVisible)
+                win1.Close();
+            else
+                win1.Show();
+        }
+    }
+}
